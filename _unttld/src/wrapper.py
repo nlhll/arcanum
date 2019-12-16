0aa8620deca9c35df9734b2913b684a85d1f6935
nlhll <glebslnk@gmail.com>
2019-12-16 18:30:09 +0300

    Branch rollback

---
+++ _unttld/src/wrapper.py
@@ -0,0 +1,16 @@
+from kaggleExtract import KaggleExtract
+from sqliteLoad import SQLiteLoad
+
+'''
+dataset = input('Input dataset suffix '
+                '(Dataset URL suffix in format <owner>/<dataset-name>): ')
+if not dataset:
+    file_type = input('Input primal files\' type: csv, json, sqlite. '
+                      '(Note: there might be files '
+                      'with other types inthe dataset): ')
+'''
+
+# extractor = KaggleExtract()
+# extractor.download_dataset()
+# loader = SQLiteLoad()
+# loader.load_wrapper()
